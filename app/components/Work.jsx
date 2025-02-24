91dc9fe582163680ac669015e75f428981175dd3
Ankan Bera <beraankan8@gmail.com>
2025-02-25 01:27:05 +0530

    new changes

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -37,9 +37,9 @@ const Work = ({ isDarkMode }) => {
 
       {/* ----Works cards---- */}
       <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 0.9, delay: 0.6 }}
+        initial={{ opacity: 0 , y:-20}}
+        whileInView={{ opacity: 1, y:0 }}
+        transition={{ duration: 2.4, delay: 0.6 }}
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-10 gap-5 dark:text-black">
         {workData.map((project, index) => (
           <motion.div
@@ -48,15 +48,27 @@ const Work = ({ isDarkMode }) => {
             key={index}
             className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
             style={{ backgroundImage: `url(${project.bgImage})` }}>
-            <div className="bg-white w-11/12 border border-black rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-              <div>
-                <h2 className="font-semibold">{project.title}</h2>
-                <p className="text-sm text-gray-700 ">{project.description}</p>
+            <a
+              href={project.githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={index}>
+              <div className="bg-white w-11/12 border border-black rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+                <div>
+                  <h2 className="font-semibold">{project.title}</h2>
+                  <p className="text-sm text-gray-700 ">
+                    {project.description}
+                  </p>
+                </div>
+                <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-blue-300 transition-none">
+                  <Image
+                    src={assets.send_icon}
+                    alt="send icon"
+                    className="w-5"
+                  />
+                </div>
               </div>
-              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-blue-300 transition-none">
-                <Image src={assets.send_icon} alt="send icon" className="w-5" />
-              </div>
-            </div>
+            </a>
           </motion.div>
         ))}
       </motion.div>
@@ -64,7 +76,7 @@ const Work = ({ isDarkMode }) => {
       <motion.a
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
-        transition={{ duration: 1.1, delay: 0.5 }}
+        transition={{ duration: 1, delay: 1.1 }}
         href="https://github.com/Ankan-B10?tab=repositories"
         className="w-max flex items-center justify-center gap-2 text-gray-900 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 bg-white dark:text-black">
         Show more
--- assets/assets.js
+++ assets/assets.js
@@ -1,129 +1,191 @@
-import user_img from './user-img.jpeg';
-import code_icon from './code-icon.png';
-import code_icon_dark from './code-icon-dark.png';
-import edu_icon from './edu-icon.png';
-import edu_icon_dark from './edu-icon-dark.png';
-import project_icon from './project-icon.png';
-import project_icon_dark from './project-icon-dark.png';
-import vscode from './vscode.png';
-import firebase from './firebase.png';
-import figma from './figma.png';
-import git from './git.png';
-import mongodb from './mongodb.png';
-import right_arrow_white from './right-arrow-white.png';
-import logo from './logo.png';
-import mail_icon from './mail_icon.png';
-import mail_icon_dark from './mail_icon_dark.png';
-import profile_img from './profile-img.png';
-import download_icon from './download-icon.png';
-import hand_icon from './hand-icon.png';
-import header_bg_color from './header-bg-color.png';
-import moon_icon from './moon_icon.png';
-import sun_icon from './sun_icon.png';
-import arrow_icon from './arrow-icon.png';
-import arrow_icon_dark from './arrow-icon-dark.png';
-import menu_black from './menu-black.png';
-import menu_white from './menu-white.png';
-import close_black from './close-black.png';
-import close_white from './close-white.png';
-import web_icon from './web-icon.png';
-import mobile_icon from './mobile-icon.png';
-import ui_icon from './ui-icon.png';
-import graphics_icon from './graphics-icon.png';
-import right_arrow from './right-arrow.png';
-import send_icon from './send-icon.png';
-import right_arrow_bold from './right-arrow-bold.png';
-import right_arrow_bold_dark from './right-arrow-bold-dark.png';
-import C_Plus from './C++_logo.png';
-import python from './python.png';
-import next from './nextjs.png';
-import jupyter from './jupyter.png';
-import kali from './kali.png';
-import canva from './canva.png';
-import teaching_icon from './education-logo.png'; 
+import user_img from "./user-img.jpeg";
+import code_icon from "./code-icon.png";
+import code_icon_dark from "./code-icon-dark.png";
+import edu_icon from "./edu-icon.png";
+import edu_icon_dark from "./edu-icon-dark.png";
+import project_icon from "./project-icon.png";
+import project_icon_dark from "./project-icon-dark.png";
+import vscode from "./vscode.png";
+import firebase from "./firebase.png";
+import figma from "./figma.png";
+import git from "./git.png";
+import mongodb from "./mongodb.png";
+import right_arrow_white from "./right-arrow-white.png";
+import logo from "./logo.png";
+import mail_icon from "./mail_icon.png";
+import mail_icon_dark from "./mail_icon_dark.png";
+import profile_img from "./profile-img.png";
+import download_icon from "./download-icon.png";
+import hand_icon from "./hand-icon.png";
+import header_bg_color from "./header-bg-color.png";
+import moon_icon from "./moon_icon.png";
+import sun_icon from "./sun_icon.png";
+import arrow_icon from "./arrow-icon.png";
+import arrow_icon_dark from "./arrow-icon-dark.png";
+import menu_black from "./menu-black.png";
+import menu_white from "./menu-white.png";
+import close_black from "./close-black.png";
+import close_white from "./close-white.png";
+import web_icon from "./web-icon.png";
+import mobile_icon from "./mobile-icon.png";
+import ui_icon from "./ui-icon.png";
+import graphics_icon from "./graphics-icon.png";
+import right_arrow from "./right-arrow.png";
+import send_icon from "./send-icon.png";
+import right_arrow_bold from "./right-arrow-bold.png";
+import right_arrow_bold_dark from "./right-arrow-bold-dark.png";
+import C_Plus from "./C++_logo.png";
+import python from "./python.png";
+import next from "./nextjs.png";
+import jupyter from "./jupyter.png";
+import kali from "./kali.png";
+import canva from "./canva.png";
+import teaching_icon from "./education-logo.png";
 
 export const assets = {
-    teaching_icon,
-    canva,
-    kali,
-    jupyter,
-    next,
-    python,
-    C_Plus,
-    user_img,
-    code_icon,
-    code_icon_dark,
-    edu_icon,
-    edu_icon_dark,
-    project_icon,
-    project_icon_dark,
-    vscode,
-    firebase,
-    figma,
-    git,
-    mongodb,
-    right_arrow_white,
-    logo,
-    mail_icon,
-    mail_icon_dark,
-    profile_img,
-    download_icon,
-    hand_icon,
-    header_bg_color,
-    moon_icon,
-    sun_icon,
-    arrow_icon,
-    arrow_icon_dark,
-    menu_black,
-    menu_white,
-    close_black,
-    close_white,
-    web_icon,
-    mobile_icon,
-    ui_icon,
-    graphics_icon,
-    right_arrow,
-    send_icon,
-    right_arrow_bold,
-    right_arrow_bold_dark
+  teaching_icon,
+  canva,
+  kali,
+  jupyter,
+  next,
+  python,
+  C_Plus,
+  user_img,
+  code_icon,
+  code_icon_dark,
+  edu_icon,
+  edu_icon_dark,
+  project_icon,
+  project_icon_dark,
+  vscode,
+  firebase,
+  figma,
+  git,
+  mongodb,
+  right_arrow_white,
+  logo,
+  mail_icon,
+  mail_icon_dark,
+  profile_img,
+  download_icon,
+  hand_icon,
+  header_bg_color,
+  moon_icon,
+  sun_icon,
+  arrow_icon,
+  arrow_icon_dark,
+  menu_black,
+  menu_white,
+  close_black,
+  close_white,
+  web_icon,
+  mobile_icon,
+  ui_icon,
+  graphics_icon,
+  right_arrow,
+  send_icon,
+  right_arrow_bold,
+  right_arrow_bold_dark,
 };
 
 export const workData = [
-    {
-        title: 'PaySync - Payment',
-        description: 'NPM library',
-        bgImage: '/work-1.jpeg',
-    },
-    {
-        title: 'House Price Prediction',
-        description: 'ML Project',
-        bgImage: '/work-2.png',
-    },
-    {
-        title: 'Photography site',
-        description: 'Web Design',
-        bgImage: '/work-3.png',
-    },
-    {
-        title: 'UI/UX designing',
-        description: 'UI/UX Design',
-        bgImage: '/work-4.png',
-    },
-]
+  {
+    title: "PaySync - Payment",
+    description: "NPM library",
+    bgImage: "/work-1.jpeg",
+    githubLink: "https://github.com/Ankan-B10/PaySync",
+  },
+  {
+    title: "House Price Prediction",
+    description: "ML Project",
+    bgImage: "/work-2.png",
+    githubLink: "https://github.com/Ankan-B10/House_Price_Prediction",
+  },
+  {
+    title: "3d Portfolio",
+    description: "Web Design",
+    bgImage: "/work-3.png",
+    githubLink: "https://github.com/Ankan-B10/3d_Portfolio",
+  },
+  {
+    title: "AWS Polly & S3",
+    description: "Text-to-Speech Converter",
+    bgImage: "/work-4.png",
+    githubLink: "https://github.com/Ankan-B10/AWS-Text-to-Speech",
+  },
+  {
+    title: "Chess Game - Backend",
+    description: "Multiplayer in Localhost",
+    bgImage: "/work-5.png",
+    githubLink: "https://github.com/Ankan-B10/Chess-Game",
+  },
+  {
+    title: "Apple Vision Pro",
+    description: "Fully Static website",
+    bgImage: "/work-6.jpg",
+    githubLink: "https://github.com/Ankan-B10/Apple-Vision",
+  },
+];
 
 export const serviceData = [
-    { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming.I deliver stunning, user-friendly websites to establish your online presence. From simple sites to complex e-commerce platforms', link: '' },
-    { icon: assets.teaching_icon, title: 'Teaching Service', description: 'Teaching is like my Passion. I love to share my knowledge with others. So I teach DSA in C++ to my jouniors', link: '' },
-    { icon: assets.graphics_icon, title: 'Graphics design', description: 'I bring ideas to life through captivating and meaningful designs. With a focus on creativity, strategy, and attention to detail, I create visuals that not only look great but also tell compelling stories and resonate with the audience', link: '' },
-    { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience, buiding website designs as the customer wants', link: '' },
-]
+  {
+    icon: assets.web_icon,
+    title: "Web design",
+    description:
+      "Web development is the process of building, programming.I deliver stunning, user-friendly websites to establish your online presence. From simple sites to complex e-commerce platforms",
+    link: "",
+  },
+  {
+    icon: assets.teaching_icon,
+    title: "Teaching Service",
+    description:
+      "Teaching is like my Passion. I love to share my knowledge with others. So I teach DSA in C++ to my jouniors",
+    link: "",
+  },
+  {
+    icon: assets.graphics_icon,
+    title: "Graphics design",
+    description:
+      "I bring ideas to life through captivating and meaningful designs. With a focus on creativity, strategy, and attention to detail, I create visuals that not only look great but also tell compelling stories and resonate with the audience",
+    link: "",
+  },
+  {
+    icon: assets.ui_icon,
+    title: "UI/UX design",
+    description:
+      "UI/UX design focuses on creating a seamless user experience, buiding website designs as the customer wants",
+    link: "",
+  },
+];
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'C++, HTML, CSS, JavaScript, React Js, Next Js, Python' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science (2026)' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects, Participate in many Hackathons' }
+  {
+    icon: assets.code_icon,
+    iconDark: assets.code_icon_dark,
+    title: "Languages",
+    description: "C++, HTML, CSS, JavaScript, React Js, Next Js, Python",
+  },
+  {
+    icon: assets.edu_icon,
+    iconDark: assets.edu_icon_dark,
+    title: "Education",
+    description: "B.Tech in Computer Science (2026)",
+  },
+  {
+    icon: assets.project_icon,
+    iconDark: assets.project_icon_dark,
+    title: "Projects",
+    description: "Built more than 5 projects, Participate in many Hackathons",
+  },
 ];
 
 export const toolsData = [
-    assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git, assets.jupyter, assets.canva,assets.kali
+  assets.vscode,
+  assets.firebase,
+  assets.mongodb,
+  assets.figma,
+  assets.git,
+  assets.jupyter,
+  assets.canva,
+  assets.kali,
 ];
